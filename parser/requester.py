3fc5e0818897210259cf7510315b95b541bfe1b2
Deformater <muravenkog@mail.ru>
2022-08-26 14:57:04 +0300

    fix(requester): change host

--- parser/requester.py
+++ parser/requester.py
@@ -1,14 +1,15 @@
 import requests
 import json
 
-with open('startups.json') as f:
-    data_list = json.load(f)
+# with open('startups.json') as f:
+#     data_list = json.load(f)
     
-for data in data_list:
-    response = requests.post('https://api.drivehack.cupsoft.ru/startup/force', json=data)
+# for data in data_list:
+#     response = requests.post('http://10.8.0.2:8000/startup/force', json=data)
 
 with open('all_transport_articles_from_first_100_pages_techrunch.json') as f:
     data_list = json.load(f)
     
 for data in data_list:
-    response = requests.post('https://api.drivehack.cupsoft.ru/startups', json=data)
+    response = requests.post('http://10.8.0.2:8000/startups', json=data)
+    print(response.status_code)
